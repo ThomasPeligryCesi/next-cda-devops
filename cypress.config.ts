9837259fe6d8ac8a4ea2465d4f7bb378e35a07be
Thomas Peligry <thomas.peligry@viacesi.fr>
2026-04-21 13:55:20 +0200

    rebase sur le git d'Alexis

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents() {},
+    setupNodeEvents(on, config) {},
   },
   component: {
     devServer: {
-      module: "es2015",
       framework: "next",
       bundler: "webpack",
     },
